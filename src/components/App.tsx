96af7a49f6ab5f9a3d535ce1207aacdbaec3ac9f
Nathan <79818190+ncyarnall@users.noreply.github.com>
2022-04-23 17:59:29 -0400

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import Statuses from './Statuses';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Statuses />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div>
+           <div className="bg-red-900 bg-cover bg-no-repeat bg-center  rounded-xl my-3 mx-3 ">
+                <h1 className="text-yellow-500 text-center py-5 px-2 text-5xl">SUMMIT JUNIOR BASEBALL SOFTBALL</h1>
+           </div>
+        </div>
+    );
+}
+
+export default Footer;
