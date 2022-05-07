133e23574785e1ee9421bccaba982f3571406c72
Nathan <79818190+ncyarnall@users.noreply.github.com>
2022-05-06 23:52:40 -0400

    Add files via upload

---
+++ src/components/Alert.tsx
@@ -0,0 +1,12 @@
+
+function Alert() {
+    return (
+        <div className="text-center text-gray-800 py-2.5 px-2.5 text-lg">
+            <p>
+                See which fields are open or closed at anytime with Field Status - updates every 5 minutes
+            </p>
+        </div>
+    )
+}
+
+export default Alert;
