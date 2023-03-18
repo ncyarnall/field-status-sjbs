7f275bea936a4ecd7c3035f376fc6f62c0e5d839
Nathan <79818190+ncyarnall@users.noreply.github.com>
2023-03-18 18:26:41 -0400

    Delete Statuses.tsx

--- src/components/Statuses.tsx
+++
@@ -1,9 +0,0 @@
-function Statuses () {
-    return (
-        <div className="bg-[url('../public/field2.jpeg')] flex items-center justify-center bg-red-900 h-auto py-12 px-1 ">
-            <iframe title="statuses" className="w-1.1kp h-537p rounded-xl" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWa1wY3JlqaR9-5cZB9PsFDPQPHyIMFuJiK1ySTpUbf8lMs0PLXh5Q7S1KbVEZsHjTuJG4Yvx19LGv/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-        </div>
-    )
-}
-
-export default Statuses;
