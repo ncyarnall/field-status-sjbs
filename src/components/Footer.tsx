963882a519fb220afac7fc30d3dd75dcb37c9b3d
Nathan <79818190+ncyarnall@users.noreply.github.com>
2023-03-22 17:31:08 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
                 <div className="flex items-center justify-center">
                     <a href="https://www.summitsjbs.org/" className="flex items-center mb-4 sm:mb-0">
                         <img src="https://www.summitssbl.org/wp-content/uploads/sites/2318/2022/08/Summit-SB-Logo-3-e1660592918493.png" className="mr-3 h-70p" alt="SJBS Logo" />
-                        <span className="self-center text-3xl font-semibold whitespace-wrap text-yellow-500">Summit Baseball & Softball League</span>
+                        <span className="self-center text-3xl font-semibold whitespace-wrap text-yellow-500">Summit Softball & Baseball League</span>
                     </a>
                 </div>
             </footer>
